d4b22ca13336e3ac13f32fb8fe1dcfb2ffa7672e
Gulshan Kumar <newgulshandps09@gmail.com>
2024-01-04 00:50:17 +0530

    First Commit

---
+++ var.py
@@ -0,0 +1,26 @@
+#string
+name = "Gulshan"
+print(type(name))
+
+#integer
+roll_number = 56
+print(type(roll_number))
+
+#floating number
+percentage = 95.8
+print(type(percentage))
+
+#boolean
+is_student = True
+print(type(is_student))
+
+print(name,roll_number,percentage,is_student)
+
+'''
+Output :
+<class 'str'>
+<class 'int'>
+<class 'float'>
+<class 'bool'>
+Gulshan 56 95.8 True
+'''
