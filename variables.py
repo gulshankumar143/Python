67c30cd1312df105fc64af3a145b7435215100b7
Gulshan Kumar <newgulshandps09@gmail.com>
2024-01-04 00:50:38 +0530

    First commit

---
+++ variables.py
@@ -0,0 +1,23 @@
+#string
+name = "Gulshan"
+print(name)
+
+#integer
+roll_number = 56
+print(roll_number)
+
+#floating number
+percentage = 95.8
+print(percentage)
+
+#boolean
+is_student = True
+print(is_student)
+
+'''
+Output :
+Gulshan
+56
+95.8
+True
+'''
