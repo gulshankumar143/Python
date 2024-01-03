77d5dd90385819e80795568b5fcaae70f8490986
Gulshan Kumar <newgulshandps09@gmail.com>
2024-01-04 00:50:29 +0530

    First commit

---
+++ var1.py
@@ -0,0 +1,15 @@
+name = "Gulshan"
+roll_number = 56
+percentage = 95.8
+is_student = True
+print(name,roll_number,percentage,is_student)
+print("My name is " + name + " and my roll number is ",roll_number,".")
+print("I scored ",percentage,"%ge in my final exam. I am a student is",is_student,".")
+
+'''
+    Output :
+    Gulshan 56 95.8 True
+    My name is Gulshan and my roll number is  56 .
+    I scored  95.8 %ge in my final exam. I am a student is True .
+
+'''
